3736e46f51f6571d161ac91cdd23ce96b3b5d922
NenyaBit <nenya-bit@proton.me>
2025-06-07 22:20:21 +0200

    Compile fixes: Bring StringUtil.h from DDR

--- src/Util/StringUtil.h
+++ src/Util/StringUtil.h
@@ -1,28 +1,57 @@
 #pragma once
 
+#include <ranges>
+
 namespace Utility
 {
+#pragma warning(push)
+#pragma warning(disable : 4244)
 #define STR_TRANSFORM(f) std::transform(str.cbegin(), str.cend(), str.begin(), [](int c) { return f(c); });
 
 	template <class T>
-	constexpr void ToLower(T& str)	{		STR_TRANSFORM(std::tolower);	}
-	template <class T>
-	constexpr void ToUpper(T& str)	{		STR_TRANSFORM(std::toupper);	}
-	template <class T>
-	constexpr T CastLower(T str) { ToLower(str); return str; }
-	template <class T>
-	constexpr T CastUpper(T str) { ToUpper(str); return str; }
-
-#undef STR_TRANSFORM
-
-  template <class T>
-	inline std::vector<T> StringSplit(T& a_str, std::string_view a_delimiter)
+	constexpr void ToLower(T& str)
 	{
-		auto range = a_str | std::ranges::views::split(a_delimiter);
-		return { range.begin(), range.end() };
+		STR_TRANSFORM(std::tolower);
+	}
+	template <class T>
+	constexpr void ToUpper(T& str)
+	{
+		STR_TRANSFORM(std::toupper);
+	}
+	constexpr std::string CastLower(std::string str)
+	{
+		ToLower(str);
+		return str;
+	}
+	constexpr std::string CastUpper(std::string str)
+	{
+		ToUpper(str);
+		return str;
 	}
 
-  template <class T>
+#undef STR_TRANSFORM
+#pragma warning(pop)
+
+	inline std::vector<std::string_view> StringSplit(const std::string_view& a_view, const std::string_view& a_delim)
+	{
+		namespace views = std::ranges::views;
+		return a_view | views::split(a_delim) | views::transform([](auto&& subrange) {
+			auto word = std::string_view(&*subrange.begin(), std::ranges::distance(subrange));
+			while (!word.empty() && std::isspace(word.front()))
+				word.remove_prefix(1);
+			while (!word.empty() && std::isspace(word.back()))
+				word.remove_suffix(1);
+			return word;
+		}) | views::filter([](auto&& word) { return !word.empty(); }) | std::ranges::to<std::vector>();
+	}
+
+	inline std::vector<std::string> StringSplitToOwned(const std::string_view& a_view, const std::string_view& a_delim)
+	{
+		const auto ret = StringSplit(a_view, a_delim);
+		return std::vector<std::string>(ret.cbegin(), ret.cend());
+	}
+
+    template <class T>
 	static inline std::string StringJoin(const std::vector<T>& a_vec, std::string_view a_delimiter)
 	{
 		return std::accumulate(a_vec.begin(), a_vec.end(), std::string{},
@@ -33,7 +62,7 @@ namespace Utility
 
 	static inline bool IsNumericString(const std::string& a_str)
 	{
-		static const std::regex pattern{ R"(^[+-]?(?:\d+|\d*\.\d+)$)" };
+		static const std::regex pattern{ R"(^[+-]?(?:(0x)?[0-9A-Fa-f]+|\d+|\d*\.\d+)$)" };
 		return std::regex_match(a_str, pattern);
 	}
 
@@ -48,6 +77,11 @@ namespace Utility
 		for (size_t index = str.find(substr1, 0); index != std::string::npos && substr1.length(); index = str.find(substr1, index + substr2.length()))
 			str.replace(index, substr1.length(), substr2);
 		return str;
+	}
+
+	static inline std::string RemovePrefix(std::string a_str, std::string a_prefix)
+	{
+		return a_str.starts_with(a_prefix) ? a_str.substr(a_prefix.size()) : a_str;
 	}
 
 }	 // namespace String
